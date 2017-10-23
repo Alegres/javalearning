e767685047ebc2a626ba564cdadff1e498a584da
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 17:15:48 +0200

    id #initInfo element is now being filled by counted average at the start

--- js/init.js
+++ js/init.js
@@ -1,1 +1,6 @@
 /* This script will perform some init actions */
+
+var initInfo = document.getElementById("initInfo");
+var av = getAverage(5, 10);
+
+initInfo.innerHTML += av.toString() + "<br />";
