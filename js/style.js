c25d50ecee9e67cc5003a66be6df1db12fcb47e9
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 18:06:49 +0200

    Ive changed some css parameters using javascript

--- js/style.js
+++ js/style.js
@@ -1,1 +1,5 @@
 /* This script will do some style manipulation */
+var title = document.getElementById("introduction");
+
+title.setAttribute("style", "color: blue;");
+title.style.background = "#bbb";
