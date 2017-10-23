e4add67e7f36ffddbfebcf47458fc14e07e8e39e
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 18:27:08 +0200

    Added some basic onclick and onmouseover events

--- js/events.js
+++ js/events.js
@@ -1,1 +1,13 @@
 /* This script will create some events */
+var introduction = document.getElementById("introduction");
+
+introduction.onclick = function() {
+    this.innerHTML = "Klikniete";
+}
+
+var list = document.getElementById("list");
+var el = list.getElementsByTagName("li");
+
+el[0].onmouseover = function() {
+    this.innerHTML = "Najechales";
+}
