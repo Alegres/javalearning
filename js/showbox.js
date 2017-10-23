b30d7829beb26f57179d5e762367a3bf79b40fa8
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 19:18:30 +0200

    Added onload event

--- js/showbox.js
+++ js/showbox.js
@@ -1,13 +1,23 @@
 /* Some js for my showbox */
-var messagebox = document.getElementById("message");
-var button = document.getElementById("btn1");
 
-button.onclick = function() {
-    if(messagebox.className == "open") {
-        messagebox.className = "";
-        button.innerHTML = "Show more";
-    } else {
-        messagebox.className = "open";
-        button.innerHTML = "Hide message";
-    }
+function setUpEvents() {
+    
+    var messagebox = document.getElementById("message");
+    var button = document.getElementById("btn1");
+
+    button.onclick = function() {
+        if(messagebox.className == "open") {
+            messagebox.className = "";
+            button.innerHTML = "Show more";
+        } else {
+            messagebox.className = "open";
+            button.innerHTML = "Hide message";
+        }
+    }   
+    
+}
+
+
+window.onload = function() {
+    setUpEvents();
 }
