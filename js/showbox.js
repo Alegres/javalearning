900801a8bf72c27e42b8fe79df9806f9e5fb795f
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 19:13:50 +0200

    Expanding the message box now works like a charm!

--- js/showbox.js
+++ js/showbox.js
@@ -1,1 +1,13 @@
 /* Some js for my showbox */
+var messagebox = document.getElementById("message");
+var button = document.getElementById("btn1");
+
+button.onclick = function() {
+    if(messagebox.className == "open") {
+        messagebox.className = "";
+        button.innerHTML = "Show more";
+    } else {
+        messagebox.className = "open";
+        button.innerHTML = "Hide message";
+    }
+}
