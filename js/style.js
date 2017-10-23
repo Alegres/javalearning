8936e88c48c16d9f70caff10a1883d81204f60b7
Alegres <32520354+Alegres@users.noreply.github.com>
2017-10-23 17:58:54 +0200

    Prepared some background for style modifications

---
+++ js/style.js
@@ -0,0 +1,8 @@
+/* This script will demonstrate some string actions in JS */
+var str = "Hello, world!";
+
+var sliced = str.slice(str.indexOf("world"), str.length);
+
+var diavola = "Sos San Marzano DOP, mozarella, salami Spianata, parmezan";
+
+var pizzaArray = diavola.split(",");
